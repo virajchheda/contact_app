e70dc2dd1bc39af229079d6b6aa6124e676d0624
virajchheda <viraj.chheda@mirraw.com>
2020-12-14 04:26:59 +0530

    comments added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -22,6 +22,7 @@ class ContactsController < ApplicationController
   end
   
   def set_locale
+    #for localization - change_locale will be present if language is selected from dropdowm from view
     redirect_to root_path(locale: params[:change_locale]) if params[:change_locale]
     extract_locale = params[:locale].present? && I18n.available_locales.include?(params[:locale].to_sym) && params[:locale].to_sym
     I18n.locale = extract_locale || I18n.default_locale
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email, :message
   after_commit :send_contact_detail_mail, on: :create
 
+  #to send mail in callback
   def send_contact_detail_mail
     ContactMailer.send_contact_details(self).deliver_later
   end
