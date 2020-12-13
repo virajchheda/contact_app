5c9dde85b2f4d92e191c935b2b649ee9585c450e
virajchheda <viraj.chheda@mirraw.com>
2020-12-14 04:10:57 +0530

    removed inline js in index.html.haml and proper placement of asset files

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -31,17 +31,22 @@
     });
 
     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
+      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+              return false;
+          }
+      }
+      else if($(input).attr('name') == 'phone_number' &&  $(input).val().trim()!= '')  {
+          if ($(input).val().trim().match(/^(\+\d{10,15})$/) == null){
+              return false;
         }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
+      }
+      else {
+          if($(input).val().trim() == '' && $(input).attr('name') != 'phone_number'){
+              return false;
+          }
+      }
+  }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
